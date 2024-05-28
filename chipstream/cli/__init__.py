41444ddd13d150595ba0da77722aae2ac345ee6a
Paul Müller <dev@craban.de>
2024-05-28 09:21:43 +0200

    enh: make chipstream.gui and chipstream.cli executable as a package

--- chipstream/cli/__init__.py
+++ chipstream/cli/__init__.py
@@ -1,10 +1,1 @@
-try:
-    import click
-except ImportError:
-    click = None
-
-if click is None:
-    def main(*args, **kwargs):
-        print("Please install the 'click' Python package to access the CLI!")
-else:
-    from .cli_main import chipstream_cli as main  # noqa: F401
+from ._main import main  # noqa: F401
